442d0cec137968e0104bbbfdb1a5907720bc087d
ChoiSW99 <seungwoo714@gmail.com>
2023-05-30 02:10:08 +0900

    ChargeShootBomb.cs 생성 및 GameManager 싱글톤 구현

---
+++ Assets/02.Scripts/CSW_scripts/ChargeShootBomb.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeShootBomb : MonoBehaviour
+{
+    private GameManager gameManager;
+    private int[] remainingBomb;
+    private TMPro.TextMeshProUGUI[] TMP_bombs;
+
+    public bool isCharging;
+
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+        remainingBomb = gameManager.remainingBomb;
+        TMP_bombs = gameManager.TMP_bombs;
+
+        isCharging = false;
+    }
+
+    public void Charge()
+    {
+        isCharging = true;
+        StartCoroutine(ShootCharging());
+
+    }
+
+    public void Shoot()
+    {
+        isCharging = false;
+        
+        if (gameManager.remainingBomb[gameManager.loadedBombNumber] > 0)
+        {
+            gameManager.remainingBomb[gameManager.loadedBombNumber]--;
+            gameManager.TMP_bombs[gameManager.loadedBombNumber].text = gameManager.remainingBomb[gameManager.loadedBombNumber].ToString();
+            Debug.Log("Shoot");
+        }
+        else
+            Debug.Log("Cannot Shoot Bomb. no more bomb.");
+    }
+
+    private IEnumerator ShootCharging()
+    {
+        while (isCharging)
+        {
+            Debug.Log("ShootCharging");
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        yield break;
+    }
+}
--- Assets/02.Scripts/CSW_scripts/GameManager.cs
+++ Assets/02.Scripts/CSW_scripts/GameManager.cs
@@ -6,10 +6,17 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    // 싱글톤 인스턴스
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+
     [Header("Bomb")]
     [Space]
-    public int[] remainingBomb;
+    public Slider chargingSlider;
+    public float chargingSpeed = 1f; // 충전 속도
+    private float currentCharge = 0f; // 현재 충전 상태
 
+    public int[] remainingBomb;
 
     public TextMeshProUGUI[] TMP_bombs;
 
@@ -25,11 +32,25 @@ public class GameManager : MonoBehaviour
     public int loadedBombNumber;
     private int bombButtonLength;
 
+
+    private void Awake()
+    {
+        if (instance == null)   // GameManager 인스턴스를 생성하고 현재 인스턴스가 없을 경우에만 할당
+            instance = this;
+        else
+            Destroy(gameObject);    // 이미 인스턴스가 존재하는 경우, 현재 객체를 파괴하여 중복 생성 방지
+    }
+    private void OnDestroy()
+    {
+        if (instance == this)           // 인스턴스가 파괴될 때 현재 인스턴스를 해제
+            instance = null;
+    }
+
     private void Start()
     {
         bombButtonLength = bombButtons.Length;
-
-        for(int i=0; i< bombButtonLength; i++)
+        
+        for (int i=0; i< bombButtonLength; i++)
         {
             TMP_bombs[i].text = remainingBomb[i].ToString();
         }
@@ -78,15 +99,5 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void Shoot()
-    {
-        if(remainingBomb[loadedBombNumber] > 0)
-        {
-            remainingBomb[loadedBombNumber]--;
-            TMP_bombs[loadedBombNumber].text = remainingBomb[loadedBombNumber].ToString();
-            Debug.Log("Shoot");
-        }
-        else
-            Debug.Log("Cannot Shoot Bomb. no more bomb.");
-    }
+    
 }
