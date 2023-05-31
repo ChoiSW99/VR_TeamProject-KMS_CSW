5f4d79f26838fcd2a31f93510f0e8cc9b740a959
KMS <seokk98@naver.com>
2023-05-31 19:09:43 +0900

    Head Rotate
    05.31 19:09

--- Assets/02.Scripts/KMS_scripts/Headmove.cs
+++ Assets/02.Scripts/KMS_scripts/Headmove.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
 public class Headmove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public InputActionReference joystickInputAction;  // 조이스틱 입력 액션 참조
+
+    private void OnEnable()
     {
-        
+        joystickInputAction.action.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        joystickInputAction.action.Disable();
+    }
+
+    private void Update()
+    {
+        HandleRotation();
+    }
+
+    private void HandleRotation()
+    {
+        Vector2 joystickInput = joystickInputAction.action.ReadValue<Vector2>();
+
+        float rotationSpeed = 10f;  // 회전 속도 조절을 위한 값
+
+        float rotationAmount = joystickInput.x * rotationSpeed * Time.deltaTime;  // X축 입력을 기반으로 회전량 계산
+
+        transform.Rotate(Vector3.up, rotationAmount);
     }
 }
